db11543682899d510bcab32b16d36148d2aeb4f0
textualbois <wivna360@hotmail.com>
2024-06-25 00:09:06 +0200

    sctivities fix

--- philo/philo_activities/philo_activities.c
+++ philo/philo_activities/philo_activities.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   philo_activities.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ivansemin <ivansemin@student.42.fr>        +#+  +:+       +#+        */
+/*   By: isemin <isemin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/23 01:55:21 by ivansemin         #+#    #+#             */
-/*   Updated: 2024/06/24 03:01:49 by ivansemin        ###   ########.fr       */
+/*   Updated: 2024/06/24 22:34:09 by isemin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../philo.h"
+#include "philo.h"
 
 void	think(t_philosopher *philo)
 {
@@ -22,26 +22,36 @@ void	pick_up_forks(t_philosopher *philo)
 	if (has_starved(philo))
 		philo_dead(philo);
 
-	pthread_mutex_lock(philo->left_fork->mutex); // this can take time
-	if (has_starved(philo))	// so check again if philo starved
-		philo_dead(philo);
+	if (philo->id % 2 == 0)
+	{
+		pthread_mutex_lock(philo->left_fork->mutex); // this can take time
+		if (has_starved(philo))	// so check again if philo starved
+			philo_dead(philo);
+		print_action(TAKING_FORK, philo); // this also can take time
+	}
 
-	print_action(TAKING_FORK, philo); // this also can take time
 	if (has_starved(philo))	// so check again if philo starved
 		philo_dead(philo);
 
 	pthread_mutex_lock(philo->right_fork->mutex); // this can also take time
 	if (has_starved(philo)) // so check again if starved
 		philo_dead(philo);
-
 	print_action(TAKING_FORK, philo); // this also can take time
-	// but it doesnt matter, becausae the is going to eat
 
+	if (philo->id % 2 != 0)
+	{
+		pthread_mutex_lock(philo->left_fork->mutex); // this can take time
+		if (has_starved(philo))	// so check again if philo starved
+			philo_dead(philo);
+		print_action(TAKING_FORK, philo);
+	}
 }
 
 void	eat(t_philosopher *philo)
 {
-	usleep(philo->meta->time_to_eat);
+	philo->last_meal_ms = time_in_ms();
+	print_action(EATING, philo);
+	sleep_ms(philo->meta->time_to_eat);
 	philo->times_eaten += 1;
 }
 
@@ -51,8 +61,8 @@ void	put_down_forks(t_philosopher *philo)
 	pthread_mutex_unlock(philo->right_fork->mutex);
 }
 
-void	sleep(t_philosopher *philo)
+void	philo_sleep(t_philosopher *philo)
 {
 	print_action(SLEEPING, philo);
-	usleep(philo->meta->time_to_sleep);
+	sleep_ms(philo->meta->time_to_sleep);
 }

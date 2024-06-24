62049d4726a2e2c5d791cc6d35d3d1446961ceab
textualbois <wivna360@hotmail.com>
2024-06-25 00:09:49 +0200

    utils dump

--- philo/utils/utils.c
+++ philo/utils/utils.c
@@ -6,9 +6,73 @@
 /*   By: isemin <isemin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/23 19:01:18 by isemin            #+#    #+#             */
-/*   Updated: 2024/06/23 19:01:36 by isemin           ###   ########.fr       */
+/*   Updated: 2024/06/24 18:34:02 by isemin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../philo.h"
+#include "philo.h"
 
+int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+
+long	ft_atol(const char *nptr)
+{
+	int			i;
+	long long	res;
+	long long	sign;
+
+	res = 0;
+	i = 0;
+	sign = 0;
+	while (nptr[i] == 32 || (nptr[i] >= 9 && nptr[i] <= 13))
+		i++;
+	while (nptr[i] == '+' || nptr[i] == '-')
+	{
+		if (sign != 0)
+			return (0);
+		if (nptr[i] == '-')
+			sign = -1;
+		else
+			sign = 1;
+		i++;
+	}
+	while (ft_isdigit(nptr[i]))
+	{
+		res = res * 10 + nptr[i] - 48;
+		i++;
+	}
+	return (sign * res + (sign == 0) * res);
+}
+
+int	ft_atoi(const char *nptr)
+{
+	int	i;
+	int	res;
+	int	sign;
+
+	res = 0;
+	i = 0;
+	sign = 0;
+	while (nptr[i] == 32 || (nptr[i] >= 9 && nptr[i] <= 13))
+		i++;
+	while (nptr[i] == '+' || nptr[i] == '-')
+	{
+		if (sign != 0)
+			return (0);
+		if (nptr[i] == '-')
+			sign = -1;
+		else
+			sign = 1;
+		i++;
+	}
+	while (ft_isdigit(nptr[i]))
+	{
+		res = res * 10 + nptr[i] - 48;
+		i++;
+	}
+	return (sign * res + (sign == 0) * res);
+}

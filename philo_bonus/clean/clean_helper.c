03e72d465c7dc967c86e25ae85a931741099f724
textualbois <wivna360@hotmail.com>
2024-07-07 17:16:02 +0200

    fixed bad semaphore closer

--- philo_bonus/clean/clean_helper.c
+++ philo_bonus/clean/clean_helper.c
@@ -6,7 +6,7 @@
 /*   By: isemin <isemin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 22:18:42 by isemin            #+#    #+#             */
-/*   Updated: 2024/07/07 17:13:34 by isemin           ###   ########.fr       */
+/*   Updated: 2024/07/07 17:15:52 by isemin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,18 @@ void	destroy_semaphore(sem_t *sem, const char *name)
 
 void	clean_params(t_parameters **params)
 {
+	sem_t	*death;
+
 	free((*params)->watcher);
 	destroy_semaphore((*params)->time, "/time");
 	destroy_semaphore((*params)->death_watcher, "/death_watcher");
 	destroy_semaphore((*params)->printer, "/print");
 	destroy_semaphore((*params)->forks, "/forks");
+	death = sem_open("/death", 0);
+	if (death != SEM_FAILED)
+	{
+		sen_close(death);
+		sem_unlink("/death");
+	}
 	free(*params);
 }

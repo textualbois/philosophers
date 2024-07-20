53a8cb5386659944f5fd7cd72a54dbe034165c28
textualbois <wivna360@hotmail.com>
2024-07-20 02:26:42 +0200

    added missing mutex)lock

--- philo/philo_activities/fork_helpers.c
+++ philo/philo_activities/fork_helpers.c
@@ -6,7 +6,7 @@
 /*   By: isemin <isemin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 01:23:42 by isemin            #+#    #+#             */
-/*   Updated: 2024/07/19 23:39:29 by isemin           ###   ########.fr       */
+/*   Updated: 2024/07/20 02:26:10 by isemin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,7 @@ int	pick_up_from_right(t_philosopher *philo)
 		pthread_mutex_unlock(philo->right_fork->mutex);
 		return (-1);
 	}
+	pthread_mutex_lock(philo->left_fork->mutex);
 	pthread_mutex_lock(philo->meta->global_mtx);
 	print_action(TAKING_FORK, philo);
 	pthread_mutex_unlock(philo->meta->global_mtx);
